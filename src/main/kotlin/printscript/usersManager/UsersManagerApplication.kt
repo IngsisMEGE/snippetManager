94bb6374c6284544ab2c22f9fbf03dbdbce2ee82
juancruzestevez02 <juan.estevez@ing.austral.edu.ar>
2024-05-22 16:26:56 -0300

    docker

--- src/main/kotlin/printscript/usersManager/UsersManagerApplication.kt
+++ src/main/kotlin/printscript/usersManager/UsersManagerApplication.kt
@@ -1,11 +1,18 @@
 package printscript.usersManager
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class UsersManagerApplication
+class UsersManagerApplication{
+    @Bean
+    fun dotenv(): Dotenv {
+        return Dotenv.configure().load()
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<UsersManagerApplication>(*args)
+    runApplication<UsersManagerApplication>(*args)
 }
