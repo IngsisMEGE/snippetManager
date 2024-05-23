ce1ade9f2028c1dde32431f7d980b68aaf63b12b
juancruzestevez02 <juan.estevez@ing.austral.edu.ar>
2024-05-23 19:20:02 -0300

    docker

--- src/test/kotlin/printscript/usersManager/UsersManagerApplicationTests.kt
+++ src/test/kotlin/printscript/usersManager/UsersManagerApplicationTests.kt
@@ -1,11 +1,10 @@
 package printscript.usersManager
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class UsersManagerApplicationTests {
-    @Test
+    /*@Test
     fun contextLoads() {
-    }
+    }*/
 }
